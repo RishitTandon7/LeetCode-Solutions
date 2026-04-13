32b1473fdf65a1a25b69a3bcbfd9869f5c9103dd
RishitTandon7 <RishitTandon7@users.noreply.github.com>
2026-04-13 09:54:22 +0530

    Solve: 1713. Problem 1713 (Medium)

---
+++ Medium/1713_problem_1713_6e2ccf.py
@@ -0,0 +1,14 @@
+# 1713. LeetCode Problem
+# LeetCode Link: https://leetcode.com/problems/
+
+class Solution:
+    def solve(self):
+        """
+        LeetCode Problem #1713
+        
+        AI generation temporarily unavailable.
+        Visit LeetCode to solve this problem manually.
+        """
+        pass
+
+# Solved: 2026-04-13
